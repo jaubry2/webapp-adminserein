88ee1bb7d504d79fd6aacaade5c01e3ddf87f5a2
Jules Aubry <120647406+jaubry2@users.noreply.github.com>
2026-02-09 15:41:07 +0100

    style : modificatio, des boutons

--- apps/web/app/components/Button/Layout.vue
+++ apps/web/app/components/Button/Layout.vue
@@ -1,17 +1,23 @@
 <template>
   <NuxtLink
     :to="props.to"
-    class="flex w-full items-center gap-3 rounded-full px-3 py-2.5 shadow-sm transition"
+    class="flex w-full items-center gap-3 rounded-full px-3 py-2.5 transition-transform hover:scale-110 duration-300 hover:shadow-sm"
     :class="
       isActive
-        ? 'bg-white'
+        ? 'border secondary--border--color'
         : 'primary--background--color hover:primary--background--color/40'
     "
   >
     <span class="flex h-10 w-10 items-center justify-center rounded-full">
-      <UIcon :name="props.icon" class="h-4 w-4 text-slate-700" />
+      <UIcon
+        :name="props.icon"
+        class="text-slate-700"
+        :class="isActive ? 'h-6 w-6' : 'h-4 w-4'"
+      />
     </span>
-    <span :class="isActive ? 'font-semibold' : ''">{{ props.label }}</span>
+    <span class="transition" :class="isActive ? 'font-bold text-lg ' : ''">{{
+      props.label
+    }}</span>
   </NuxtLink>
 </template>
 
